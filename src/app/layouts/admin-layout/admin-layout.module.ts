8596ebe704293b19ed370818e3c1a9e719c28d73
Chetan Naithani <naithani.chetan@gmail.com>
2018-07-24 02:05:34 +0530

    Kites4Sky Angular Demo - First Cut

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { ChartsModule } from 'ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ChartsModule,
+    NgbModule,
+    ToastrModule.forRoot()
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    NotificationsComponent,
+  ]
+})
+
+export class AdminLayoutModule {}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
